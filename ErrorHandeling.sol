c8b585e42b6119865b2ded2144cd8839c0a93d49
Niki22n <96103985+Niki22n@users.noreply.github.com>
2024-06-29 12:46:35 +0530

    Update ErrorHandeling.sol

--- ErrorHandeling.sol
+++ ErrorHandeling.sol
@@ -1,27 +1,39 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-contract AccountBalances{
-    // mapping variable here
-    mapping(address => uint) public account;
+contract Marks {
+    mapping(address => uint) private marks;
 
-    function add(uint _val) public payable {
-        require(_val > 0, "value of amount must be greater than zero");
-        account[msg.sender] += _val;
+    function setMarks(uint _marks) public {
+        require(_marks >= 0 && _marks <= 100, "Marks must be between 0 and 100");
+        marks[msg.sender] = _marks;
     }
 
-    // Function to withdraw funds
-    function withdraw(uint _val) public {
-        require(_val <= account[msg.sender], "withdraw amount is greater than balance amount");
-        account[msg.sender] -= _val;
+    function getMarks(address _student) public view returns (uint) {
+        return marks[_student];
     }
 
-    // Function to check the balance
-    function checkBalance() public view returns (uint) {
-        if(account[msg.sender] <= 0){
-             revert("balance is less than or equal to zero");
-        }
-        assert(account[msg.sender] >= 0); 
-        return account[msg.sender];
+    function grades() public view returns (string memory) {
+        uint _marks = getMarks(msg.sender);
+        assert(_marks >= 0 && _marks <= 100);
+        require(_marks > 0 && _marks <= 100, "Marks must be between 0 and 100");
+
+    if (_marks > 95) {
+        return "A+";
+    } else if (_marks > 90) {
+        return "A";
+    } else if (_marks > 80) {
+        return "B+";
+    } else if (_marks > 70) {
+        return "B";
+    } else if (_marks > 60) {
+        return "C";
+    } else if (_marks > 50) {
+        return "D";
+    } else if (_marks > 40) {
+        return "E";
+    } else {
+        revert("Student is Failed: Marks are below passing marks");
     }
+}
 }
